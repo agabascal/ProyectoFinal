2b3c771b6cfb1e2fb09cc17c09c85f2f848417ad
Alexamen96 <63235687+Alexamen96@users.noreply.github.com>
2021-02-03 18:03:29 +0100

    Principio MenuManager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Program");
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
